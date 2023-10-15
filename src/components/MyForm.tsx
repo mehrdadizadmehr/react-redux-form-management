18f0cd0916a2c2712f9e41982e4a135918466d21
@mehrdadizadmehr <izadmehr.mehrdad@gmail.com>
2023-10-15 15:55:20 +0330

    add some comments

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,29 +1,43 @@
+// Import the React library and the yup library for validation.
 import React from 'react';
 import * as yup from 'yup';
 import { useForm } from '../hooks/useForm';
-import './my-form.css'
-
+import './my-form.css';
 const validationSchema = yup.object().shape({
+  // The field1 field is required.
   field1: yup.string().required('Field 1 is required'),
+
+  // The field2 field is required.
   field2: yup.string().required('Field 2 is required'),
 });
 
+// Define the type of the form state.
 type MyFormState = {
+  // The field1 field is a string.
   field1: string;
+
+  // The field2 field is a string.
   field2: string;
 };
 
+// Define the MyForm component.
 const MyForm: React.FC = () => {
+  // Use the useForm hook to get the form state and event handlers.
   const { values, errors, isSubmitting, handleChange, handleSubmit } = useForm<MyFormState>({
+    // Set the initial values of the form state.
     initialValues: {
       field1: '',
       field2: '',
     },
+
+    // Set the validation schema for the form.
     validationSchema,
   });
 
+  // Return the form element.
   return (
     <form onSubmit={handleSubmit} className="my-form">
+      // Field 1
       <div>
         <label htmlFor="field1">Field 1</label>
         <input
@@ -37,6 +51,7 @@ const MyForm: React.FC = () => {
         <span>{errors.field1}</span>
       </div>
 
+      // Field 2
       <div>
         <label htmlFor="field2">Field 2</label>
         <input
@@ -50,6 +65,7 @@ const MyForm: React.FC = () => {
         <span>{errors.field2}</span>
       </div>
 
+      // Submit button
       <button type="submit" disabled={isSubmitting} className="my-form-button">
         Submit
       </button>
@@ -57,4 +73,5 @@ const MyForm: React.FC = () => {
   );
 };
 
+// Export the MyForm component.
 export default MyForm;
--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,55 +1,77 @@
+// Import the useState hook from React to manage the form state.
 import { useState } from 'react';
 import * as yup from 'yup';
 
 type AnyObject = Record<string, any>;
-
 interface UseFormProps<T extends AnyObject> {
+
   initialValues: T;
+
+
   validationSchema?: yup.ObjectSchema<T>;
 }
 
-
 export const useForm = <T extends AnyObject>({ initialValues, validationSchema }: UseFormProps<T>) => {
+
   const [values, setValues] = useState<T>(initialValues);
+
   const [errors, setErrors] = useState<AnyObject>({});
+
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleChange = (name: string, value: any) => {
+    // Update the form state with the new value.
     setValues((prevValues) => ({ ...prevValues, [name]: value }));
+
+    // If validation is enabled, validate the field.
     if (validationSchema) {
       validateField(name, value);
     }
   };
 
+  // Define a function to validate a form field.
   const validateField = async (name: string, value: any) => {
     try {
+
       if (validationSchema) {
         await validationSchema.validateAt(name, values);
+
+        // If the field is valid, clear the error message.
         setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
       }
     } catch (error) {
+      // If validation is enabled, set the error message for the field.
       if (validationSchema) {
-        setErrors((prevErrors) => ({ ...prevErrors, [name]: (error as yup.ValidationError).errors[0] as string }));
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [name]: (error as yup.ValidationError).errors[0] as string,
+        }));
       }
     }
   };
 
+  // Define a function to handle the form submission.
   const handleSubmit = async (event?: React.FormEvent) => {
+    // If an event is passed, prevent the default form submission behavior.
     if (event) {
       event.preventDefault();
     }
 
+    // If validation is enabled, validate all of the form fields.
     if (validationSchema) {
       const validationErrors: AnyObject = {};
 
       for (const key in values) {
         try {
+          // Validate the field against the validation schema.
           await validationSchema.validateAt(key, values);
         } catch (error) {
+          // Add the field error to the validation errors object.
           validationErrors[key] = (error as yup.ValidationError).errors[0];
         }
       }
 
+      // If there are any validation errors, display them to the user.
       if (Object.keys(validationErrors).length > 0) {
         setErrors(validationErrors);
         return;
@@ -58,11 +80,11 @@ export const useForm = <T extends AnyObject>({ initialValues, validationSchema }
 
     setIsSubmitting(true);
 
-   
 
     setIsSubmitting(false);
   };
 
+  // Return the form state and event handlers.
   return {
     values,
     errors,
